3c9bc8834a84f91ac607b99741d14f3220ef7e72
Dawson Knudtson <300078204@fvtc.edu>
2024-02-06 12:50:31 -0600

    finished dynamic welcome

--- style.js
+++ style.js
@@ -1,3 +1,25 @@
 
 alert('linking worked')
 
+const hours = new Date().getHours() // get the current hour
+
+const isMorning = hours >= 4 && hours < 12 // is it morning?
+const isAfternoon = hours >= 12 && hours < 17 // is it afternoon?
+const isEvening = hours >= 17 || hours < 4 // is it evening?
+
+const welcomeDiv = document.getElementById('welcome')
+
+let message = ''
+
+if (isMorning) {
+    message = 'good morning!'
+}
+else if (isAfternoon) {
+    message = 'good afternoon!'
+}
+else if (isEvening) {
+    message = 'good evening!'
+}
+
+ 
+welcomeDiv.textContent = message;
